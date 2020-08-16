a81ff67831e328837ae96ce035566f014c5b6e5a
angelsalvatierra <angel.salvatierra@globallogic.com>
2020-08-16 11:16:05 -0300

    Tutorial Completed

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+    // Parameters
+    [SerializeField] int breakableBlocks; // Serialized for debugging purposes
+
+    // Cached references
+    SceneLoader sceneLoader;
+
+    private void Start() {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBlocks() {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed() {
+        breakableBlocks--;
+        if (breakableBlocks <= 0) {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+}
